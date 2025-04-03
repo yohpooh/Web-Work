86f1dc1c4d2e4dd7b3b6934a6eb9a360d0bc3f97
Ram Rainier Masubay Belleza <social.bramrainier@gmail.com>
2025-04-04 00:50:08 +0800

    updated

--- Angular/website-portfolio/src/app/main/main.component.ts
+++ Angular/website-portfolio/src/app/main/main.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NgxTypedJsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
+
 export class MainComponent {
+doSmth: any;
 
 }
